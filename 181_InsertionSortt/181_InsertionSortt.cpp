3f1ca49d7e819e2e1d3bba7eaa347acf05832d98
pisboiii <hfzfahrzi@gmail.com>
2024-03-13 22:10:28 -0700

    Membuat Prosedure Insertionsort

--- 181_InsertionSortt/181_InsertionSortt.cpp
+++ 181_InsertionSortt/181_InsertionSortt.cpp
@@ -32,6 +32,34 @@ void input() {                                                  // Prosedure inp
 		cin >> arr[i];                                           // Menyimpan nilai data n kedalam Arrayw arr
 	}
 }
+ 
+void insertionsort() {                                           // Prosedure Insertionsort
+
+	int temp;                                                    // Membuat variable data temporer atau penyimpanan sementara
+	int i, j;                                                    // Membuat variable j sebagai penunda
+
+	for (i = 1; i <= n - 1; i++) {                               // 1. looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];                                           // 2. simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;                                               // 3. settiing nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp)                          // 4. looping while dimana nilai j lebih besar sama dengan 0 dan
+			// arr[j] lebih besar daripada temp                    
+		{
+			arr[j + 1] = arr[j];                                 // 4a. simpan arr[j] ke dalam variable arr [j+1}
+			j--;                                                 // 4b. decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;                                       // 5 simpan nilai temp ke dalam arr[j+1]
+
+		cout << "\nPass " << i << " : ";                         // output ke layar
+		for (int k = 0; k < n; k++) {                            // Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";                               // Output ke layar
+		}
+	}
+}
+
 
 
 int main()
