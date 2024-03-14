26830f2ae1907ce2186ecb5d2f027ec97e2c1c51
pisboiii <hfzfahrzi@gmail.com>
2024-03-13 22:07:38 -0700

    Membuat array dengan inputan variabel n

--- 181_InsertionSortt/181_InsertionSortt.cpp
+++ 181_InsertionSortt/181_InsertionSortt.cpp
@@ -3,10 +3,28 @@
 
 #include <iostream>
 using namespace std;
+int arr[20];                                                    // Membuat Array dengan panjang data 20
+int n;                                                          // Membuat Variable inputan n
 
+void input() {                                                  // Prosedure input
+	while (true)
+	{
 
+		cout << "Masukan Jumlah Data pada Array : ";            // Membuat inputan jumlah elemen Array
+		cin >> n;                                               // Memanggil Variabel inputan n
+
+		if (n <= 20) {                                          // Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";     // MEnampilkan pesan jika lebih dari 20
+		}
+	}
+	cout << endl;                                                // Membuat jarak per baris program
 
 int main()
 {
-    
+
+}
 
